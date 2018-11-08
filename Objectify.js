8d25ce74e355e8a93495cc67728fcd6e136eabfe
Michael <kasmickleva@gmail.com>
2018-11-08 07:42:35 +0100

    feat: remove unnecessary comment in file

--- Objectify.js
+++ Objectify.js
@@ -1,45 +1,4 @@
 
-
-// function showResults(data) {
-//   const element = document.querySelector('code');
-//   return element.textContent = JSON.stringify(data, null, 2);
-// }
-
-// function Objectify(array) {
-//   const results = {};
-//   array.forEach(data => {
-//     if (Array.isArray(data)) {
-//       if (!results['array']) results['array'] = [];
-//       results['array'].push(data);
-//     } else {
-//       if(!results[`${typeof(data)}`]) results[`${typeof(data)}`] = []; 
-//       results[`${typeof(data)}`].push(data);
-//     }
-//   });
-
-//   return showResults(results);
-// }
-
-
-// document.querySelector('form')
-//   .addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     let inputValue = document.querySelector('input').value;
-//     inputValue = inputValue.replace(/'/g, '"');
-
-//     try {
-//       const parsedData = JSON.parse(inputValue);
-//     } catch(err) {
-
-//     }
-
-
-//     return Objectify(parsedData);
-//   });
-
-//////////////////////////////////////////////////
-
-
 function showResults(data) {
   const element = document.querySelector('code');
   return element.textContent = JSON.stringify(data, null, 2);
